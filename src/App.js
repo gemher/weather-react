2a0073842208953e7d37738f5a22d3b765e3639e
gemher <gema.334@gmail.com>
2022-04-27 18:48:33 -0500

    Linked Github

--- src/App.js
+++ src/App.js
@@ -10,7 +10,11 @@ function App() {
         <WeatherSearch />
       </header>
       <h6>
-        <a href="#" target="_blank" rel="noreferrer">
+        <a
+          href="https://github.com/gemher/weather-react"
+          target="_blank"
+          rel="noreferrer"
+        >
           Open-source code
         </a>{" "}
         by Gema Hernandez
